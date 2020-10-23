5f55991a9c7c378e05d87574ea29c1fee9649c10
juliamj <64139476+juliamj@users.noreply.github.com>
2020-10-23 19:16:08 +0200

    adjust height for section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Nav from "./components/Nav";
 import About from "./components/About";
+import Skills from "./components/Skills";
 import Portfolio from "./components/Portfolio";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import './styles/App.css';
 import './styles/Nav.css';
 import './styles/About.css';
+import './styles/Skills.css';
 import './styles/Portfolio.css';
 import './styles/Contact.css';
 import './styles/Footer.css';
@@ -16,6 +18,7 @@ function App() {
     <div className="App">
         <Nav />
         <About />
+        <Skills />
         <Portfolio />
         <Contact />
         <Footer />
