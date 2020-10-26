fab54b82e5cd56d9bcd4ff3a9eb837e4a9d25adf
juliamj <64139476+juliamj@users.noreply.github.com>
2020-10-26 15:42:54 +0100

    add skills section

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -4,8 +4,44 @@ function Skills(props) {
     return (
         <div className="skills">
             <div className="section">
-                <h1>Skills</h1>
-                <div></div>
+                <h1 className="text-big">Skills</h1>
+                <div className="skills-flex-container">
+                    <div className="skills-flex">
+                        <div>
+                            <ul className="nobull">
+                                <li><span>JavaScript</span></li>
+                                <li><span>React.js</span></li>
+                                <li><span>HTML5</span></li>
+                                <li><span>CSS3</span></li>
+                            </ul>
+                        </div>
+                        <div>
+                            <ul className="nobull">
+                                <li><span>Bootstrap</span></li>
+                                <li><span>React Bootstrap</span></li>
+                                <li><span>Grommet</span></li>
+                                <li><span>Responsive Webdesign</span></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="skills-flex">
+                        <div>
+                            <ul className="nobull">
+                                <li><span>Node.js / Express</span></li>
+                                <li><span>RESTful API's</span></li>
+                                <li><span>MongoDB / Mongoose</span></li>
+                                <li><span>SQL</span></li>
+                            </ul>
+                        </div>
+                        <div>
+                            <ul className="nobull">
+                                <li><span>Git</span></li>
+                                <li><span>GitHub</span></li>
+                                <li><span>CMS-Contentful</span></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
