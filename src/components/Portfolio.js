727f8deec4d35ca954071150f882c59bc5c03353
juliamj <64139476+juliamj@users.noreply.github.com>
2020-10-26 23:04:17 +0100

    adjust layout for project component

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -5,17 +5,36 @@ import lola from '../images/lola1.png';
 function Portfolio(props) {
     return (
         <div className="portfolio">
-            <div className="section">
+            <div className="section-projects">
                 <h1 className="heading">My Projects</h1>
-                <Project 
-                skill1="JavaScripttest" 
-                skill2="Jripttest"
-                skill3="Jripttest"
-                skill4="Jripttest"
-                skill5="Jripttest"
-                skill6="Jripttest"
-
-                />
+                <div className="projects">
+                    <Project
+                        description="A language exchange Web Application that brings people together to learn languages."
+                        skill1="JavaScripttest"
+                        skill2="Jripttest"
+                        skill3="Jripttest"
+                        skill4="Jripttest"
+                        skill5="Jripttest"
+                        skill6="Jripttest"
+                        img={lola}
+                        preview=""
+                        live="https://lola-tandem.netlify.app/"
+                        code="https://github.com/juliamj/Lola-client"
+                    />
+                    <Project
+                        description="A language exchange Web Application that brings people together to learn languages."
+                        skill1="JavaScripttest"
+                        skill2="Jripttest"
+                        skill3="Jripttest"
+                        skill4="Jripttest"
+                        skill5="Jripttest"
+                        skill6="Jripttest"
+                        img={lola}
+                        preview=""
+                        live="https://lola-tandem.netlify.app/"
+                        code="https://github.com/juliamj/Lola-client"
+                    />
+                </div>
             </div>
         </div>
     );
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,32 +1,35 @@
 import React from 'react';
-import lola from '../images/lola1.png';
 
 function Project(props) {
     const skills = [];
-  props.skill1 ? skills.push(props.skill1) : console.log("no skill")
-  props.skill2 ? skills.push(props.skill2) : console.log("no skill")
-  props.skill3 ? skills.push(props.skill3) : console.log("no skill")
-  props.skill4 ? skills.push(props.skill4) : console.log("no skill")
-  props.skill5 ? skills.push(props.skill5) : console.log("no skill")
-  props.skill6 ? skills.push(props.skill6) : console.log("no skill")
-    
+    const skills2 = [];
+    props.skill1 ? skills.push(props.skill1) : console.log("no skill")
+    props.skill2 ? skills.push(props.skill2) : console.log("no skill")
+    props.skill3 ? skills.push(props.skill3) : console.log("no skill")
+    props.skill4 ? skills2.push(props.skill4) : console.log("no skill")
+    props.skill5 ? skills2.push(props.skill5) : console.log("no skill")
+    props.skill6 ? skills2.push(props.skill6) : console.log("no skill")
+
     return (
         <div className="project-container">
             <div className="project-left">
                 <h2>Lola</h2>
-                <p className="paragraph pl-4 pr-4">Description</p>
+                <p className="paragraph pl-4 pr-4">{props.description}</p>
                 <ul className="skills tech-stack nobull">
-                    {skills.map((skill)=>                    
-                    <li className="mr-3"><span>{skill}</span></li>)}
+                    {skills.map((skill) =>
+                        <li className="mr-3"><span>{skill}</span></li>)}
                 </ul>
-
+                <ul className="skills tech-stack nobull">
+                    {skills2.map((skill) =>
+                        <li className="mr-3"><span>{skill}</span></li>)}
+                </ul>
             </div>
             <div className="project-right">
-                <img className="project-image" src={lola} />
+                <img className="project-image" src={props.img} />
                 <div className="view-project nobull">
-                    <a href="https://lola-tandem.netlify.app/" target="_blank">preview</a>
-                    <a href="https://lola-tandem.netlify.app/" target="_blank">view live</a>
-                    <a href="https://lola-tandem.netlify.app/" target="_blank">code</a>
+                    <a href={props.preview} target="_blank">preview</a>
+                    <a href={props.live} target="_blank">view live</a>
+                    <a href={props.code} target="_blank">code</a>
                 </div>
             </div>
         </div>
