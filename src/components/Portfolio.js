5b4c4510726ea29dd1b35a836d49d2fe815803af
juliamj <64139476+juliamj@users.noreply.github.com>
2020-10-27 11:13:41 +0100

    add styling for projects tech stack

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Project from "./Project";
-import lola from '../images/lola1.png';
+import lola from '../images/lola.png';
+import colorful from '../images/colorful.png';
+import cms from '../images/CMS.png';
 
 function Portfolio(props) {
     return (
@@ -9,30 +11,37 @@ function Portfolio(props) {
                 <h1 className="heading">My Projects</h1>
                 <div className="projects">
                     <Project
+                        heading="Lola"
                         description="A language exchange Web Application that brings people together to learn languages."
-                        skill1="JavaScripttest"
-                        skill2="Jripttest"
-                        skill3="Jripttest"
-                        skill4="Jripttest"
-                        skill5="Jripttest"
-                        skill6="Jripttest"
+                        skill1="React.js"
+                        skill2="Node.js/Express"
+                        skill3="MongoDB/Mongoose"
+                        skill4="Bootstrap"
+                        skill5="React Bootstrap"
                         img={lola}
                         preview=""
                         live="https://lola-tandem.netlify.app/"
                         code="https://github.com/juliamj/Lola-client"
                     />
                     <Project
-                        description="A language exchange Web Application that brings people together to learn languages."
-                        skill1="JavaScripttest"
-                        skill2="Jripttest"
-                        skill3="Jripttest"
-                        skill4="Jripttest"
-                        skill5="Jripttest"
-                        skill6="Jripttest"
-                        img={lola}
+                        heading="Colorful"
+                        description="Experimenting with web scraping and creating a color picker with cultural information about various colors throughout history."
+                        skill1="React.js"
+                        skill2="Grommet"
+                        img={colorful}
                         preview=""
-                        live="https://lola-tandem.netlify.app/"
-                        code="https://github.com/juliamj/Lola-client"
+                        live="https://colorful-app.netlify.app/"
+                        code="https://github.com/juliamj/colorful"
+                    />
+                    <Project
+                        heading="Cookbook"
+                        description="Building a React App with Contentful as CMS."
+                        skill1="React.js"
+                        skill2="Contentful as CMS"
+                        img={cms}
+                        preview=""
+                        live="https://how-not-to-cook-recipes.netlify.app/"
+                        code="https://www.github.com/juliamj/recipes_group4"
                     />
                 </div>
             </div>
--- src/components/Project.js
+++ src/components/Project.js
@@ -2,25 +2,20 @@ import React from 'react';
 
 function Project(props) {
     const skills = [];
-    const skills2 = [];
     props.skill1 ? skills.push(props.skill1) : console.log("no skill")
     props.skill2 ? skills.push(props.skill2) : console.log("no skill")
     props.skill3 ? skills.push(props.skill3) : console.log("no skill")
-    props.skill4 ? skills2.push(props.skill4) : console.log("no skill")
-    props.skill5 ? skills2.push(props.skill5) : console.log("no skill")
-    props.skill6 ? skills2.push(props.skill6) : console.log("no skill")
+    props.skill4 ? skills.push(props.skill4) : console.log("no skill")
+    props.skill5 ? skills.push(props.skill5) : console.log("no skill")
+    props.skill6 ? skills.push(props.skill6) : console.log("no skill")
 
     return (
         <div className="project-container">
             <div className="project-left">
-                <h2>Lola</h2>
-                <p className="paragraph pl-4 pr-4">{props.description}</p>
-                <ul className="skills tech-stack nobull">
+                <h2>{props.heading}</h2>
+                <p className="description">{props.description}</p>
+                <ul className="tech-stack nobull">
                     {skills.map((skill) =>
-                        <li className="mr-3"><span>{skill}</span></li>)}
-                </ul>
-                <ul className="skills tech-stack nobull">
-                    {skills2.map((skill) =>
                         <li className="mr-3"><span>{skill}</span></li>)}
                 </ul>
             </div>
