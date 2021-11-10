45b524d301489ddf149c73ce49496f657e32fe23
juliamj <jannsen.julia@gmx.de>
2021-11-10 21:39:04 +0100

    adjust project structure and add new project

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,51 +1,64 @@
-import React from 'react';
+import React from "react";
 import Project from "./Project";
-import lola from '../images/lola.png';
-import colorful from '../images/colorful.png';
-import cms from '../images/CMS.png';
+import lola from "../images/lola.png";
+import colorful from "../images/colorful.png";
+import cms from "../images/CMS.png";
+import serokarte from "../images/serokarte.png";
 
 function Portfolio(props) {
-    return (
-        <div id="portfolio-scroll" className="portfolio">
-            <div className="section-projects">
-                <h1 className="heading">My Projects</h1>
-                <div className="projects">
-                    <Project
-                        heading="Lola"
-                        description="A language exchange Web Application that brings people together to learn languages."
-                        skill1="React.js"
-                        skill2="Node.js/Express"
-                        skill3="MongoDB/Mongoose"
-                        skill4="Bootstrap"
-                        skill5="React Bootstrap"
-                        img={lola}
-                        // preview="PREVIEW"
-                        live="https://lola-tandem.netlify.app/"
-                        code="https://github.com/juliamj/Lola-client"
-                    />
-                    <Project
-                        heading="Colorful"
-                        description="Experimenting with web scraping and creating a color picker with cultural information about various colors throughout history."
-                        skill1="React.js"
-                        skill2="Grommet"
-                        img={colorful}
-                        live="https://colorful-app.netlify.app/"
-                        code="https://github.com/juliamj/colorful"
-                    />
-                    <Project
-                        heading="Cookbook"
-                        description="Building a React App with Contentful as CMS."
-                        skill1="React.js"
-                        skill2="Contentful as CMS"
-                        img={cms}
-                        // preview=""
-                        live="https://how-not-to-cook-recipes.netlify.app/"
-                        code="https://www.github.com/juliamj/recipes_group4"
-                    />
-                </div>
-            </div>
+  return (
+    <div id="portfolio-scroll" className="portfolio">
+      <div className="section-projects">
+        <h1 className="heading">My Projects</h1>
+        <div className="projects">
+          <Project
+            heading="SERO-Karte"
+            description="SERO-karte is a crowd-sourcing project, a digital map, through which everyone is invited to share their Zero Waste insider tips in Berlin. The idea behind this map is to create a helpful tool, through which the transition towards the Zero Waste lifestyle can be as easy as possible. The project was born during a climathon in November 2020. Amongst other tasks I have been responsible for the frontend development with JavaScript and the UX & UI design using Adobe XD."
+            skill1="JavaScript"
+            skill2="Adobe XD"
+            img={serokarte}
+            // preview="PREVIEW"
+            live="https://sero-karte.de/"
+            mockup="https://xd.adobe.com/view/2cc5dbbd-b793-424d-8aad-48f69174444e-7996/"
+            prototype="https://xd.adobe.com/view/4d7fec43-82f0-42ec-ac56-e77c5705908d-182c/?fullscreen&hints=off"
+          />
+          <Project
+            heading="Lola"
+            description="A language exchange Web Application that brings people together to learn languages."
+            skill1="React.js"
+            skill2="Node.js/Express"
+            skill3="MongoDB/Mongoose"
+            skill4="Bootstrap"
+            skill5="React Bootstrap"
+            img={lola}
+            // preview="PREVIEW"
+            live="https://lola-tandem.netlify.app/"
+            code="https://github.com/juliamj/Lola-client"
+          />
+          <Project
+            heading="Colorful"
+            description="Experimenting with web scraping and creating a color picker with cultural information about various colors throughout history."
+            skill1="React.js"
+            skill2="Grommet"
+            img={colorful}
+            live="https://colorful-app.netlify.app/"
+            code="https://github.com/juliamj/colorful"
+          />
+          <Project
+            heading="Cookbook"
+            description="Building a React App with Contentful as CMS."
+            skill1="React.js"
+            skill2="Contentful as CMS"
+            skill3="Bootstrap"
+            img={cms}
+            // preview=""
+            live="https://how-not-to-cook-recipes.netlify.app/"
+            code="https://www.github.com/juliamj/recipes_group4"
+          />
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default Portfolio;
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,50 +1,89 @@
-import React, { useState } from 'react';
-import 'react-responsive-modal/styles.css';
-// import { Modal } from 'react-responsive-modal';
-// import lola from '../videos/lola.mp4';
+import React, { useState } from "react";
+import "react-responsive-modal/styles.css";
 
-function Project(props) {
-    const skills = [];
-    props.skill1 ? skills.push(props.skill1) : console.log("no skill")
-    props.skill2 ? skills.push(props.skill2) : console.log("no skill")
-    props.skill3 ? skills.push(props.skill3) : console.log("no skill")
-    props.skill4 ? skills.push(props.skill4) : console.log("no skill")
-    props.skill5 ? skills.push(props.skill5) : console.log("no skill")
-    props.skill6 ? skills.push(props.skill6) : console.log("no skill")
-    const [open, setOpen] = useState(false);
- 
-    const onOpenModal = () => setOpen(true);
-    const onCloseModal = () => setOpen(false);
-    return (
-        <div className="project-container">
-            <div className="project-left">
-                <h2>{props.heading}</h2>
-                <p className="description">{props.description}</p>
-                <ul className="tech-stack nobull">
-                    {skills.map((skill) =>
-                        <li className="mr-3"><span>{skill}</span></li>)}
-                </ul>
+function UxProject(props) {
+  const skills = [];
+  props.skill1 ? skills.push(props.skill1) : console.log("no skill");
+  props.skill2 ? skills.push(props.skill2) : console.log("no skill");
+  props.skill3 ? skills.push(props.skill3) : console.log("no skill");
+  props.skill4 ? skills.push(props.skill4) : console.log("no skill");
+  props.skill5 ? skills.push(props.skill5) : console.log("no skill");
+  props.skill6 ? skills.push(props.skill6) : console.log("no skill");
+
+  return (
+    <div className="project-container">
+      <div className="project-left">
+        <h2>{props.heading}</h2>
+        <p className="description">{props.description}</p>
+        <ul className="tech-stack nobull">
+          {skills.map((skill) => (
+            <li className="mr-3">
+              <span>{skill}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="project-right">
+        <a
+          className="image-hover"
+          href={props.live}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img className="project-image" alt="img" src={props.img} />
+        </a>
+        <div>
+          {props.code === undefined ? (
+            <div className="view-project">
+              <a
+                className="link live"
+                href={props.live}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                view live
+              </a>
+              <a
+                className="link"
+                href={props.mockup}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                mockup
+              </a>
+              <a
+                className="link"
+                href={props.prototype}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                prototype
+              </a>
             </div>
-            <div className="project-right">
-            <a className="image-hover" href={props.live} target="_blank" rel="noopener noreferrer"><img className="project-image" alt="img" src={props.img} /></a>
-                <div>
-                    {props.preview === undefined ? (<div className="view-project"><a className="link live" href={props.live} target="_blank" rel="noopener noreferrer">VIEW LIVE</a>
-                    <a className="link" href={props.code} target="_blank" rel="noopener noreferrer">CODE</a></div>) : 
-                    (<div className="view-project"><div onClick={onOpenModal} className="link pointer">{props.preview}</div>
-                    {/* <Modal open={open} onClose={onCloseModal} center>
-                      <h2>Lola</h2>                   
-                        <video width="100%" height="100%" controls autoplay>
-                            <source src={lola} type="video/mp4" />
-                                    Your browser does not support the video tag.
-                        </video>
-                    </Modal> */}
-                    <a className="link live" href={props.live} target="_blank" rel="noopener noreferrer">VIEW LIVE</a>
-                    <a className="link" href={props.code} target="_blank" rel="noopener noreferrer">CODE</a></div>)
-                    }
-                </div>
+          ) : (
+            <div className="view-project">
+              <a
+                className="link live"
+                href={props.live}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                view live
+              </a>
+              <a
+                className="link"
+                href={props.code}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                code
+              </a>
             </div>
+          )}
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
-export default Project;
+export default UxProject;
